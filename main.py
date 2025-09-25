12463ea4f3388b17540200aabc8ede8fe97b3bd6
vitorhf-dev <vitorhfranca04@gmail.com>
2025-09-25 10:18:01 -0300

    Created a .gitignore file and added code to main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_path = "books/frankenstein.txt"
+    book_text = get_book_text(file_path)
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
+
+
+     
+    
