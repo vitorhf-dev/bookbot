38db6117fc5708ad81a339638cac7add8f373c1e
vitorhf-dev <vitorhfranca04@gmail.com>
2025-09-25 12:25:59 -0300

    Adicionei uma função em stats.py para que ele conte quantos de cada caractere existe no texto, e importei a função para main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import get_book_text, word_reader, count_words
+from stats import get_book_text, word_reader, count_words, count_char
 
-main()
+
 text = word_reader()
 num_words = count_words(text)
 print(f"Found {num_words} total words")
+num_char = count_char(text)
+print(num_char)
+
+
 
 
 
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def word_reader():
 def count_words(book_text):
     words = book_text.split()
     return len(words)
+
+def count_char(book_text):
+    characters = {}
+    for ch in book_text.lower():
+        if ch in characters:
+            characters[ch] += 1
+        else:
+            characters[ch] = 1
+    return characters
+               
